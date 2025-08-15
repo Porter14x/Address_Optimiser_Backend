3267864eaf7ef748d3ebbb7b34a227dfb80a0958
Porter14x <raytporter@gmail.com>
2025-08-15 15:15:08 +0100

    Issue #1 - set up main.py with initial Flask functionality, added requirements.txt and updated gitignore to ignore my venv files

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+
+GEO_URL = "http://localhost:7070/search" #Nominatim
+ROUTE_URL = "http://localhost:8002/optimized_route" #Valhalla
+
+@app.route('/', methods=["POST"])
+def hello():
+    return "hello"
+
+if __name__=='__main__': 
+    app.run(debug=True, )
