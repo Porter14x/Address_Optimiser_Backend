cac44337da7fd9ef531825910669978378470d39
Porter14x <raytporter@gmail.com>
2025-08-16 17:16:26 +0100

    linting changes for main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Main.py is responsible for handling requests via Flask and calling functions from other modules to satisfy the requests"""
+
 from flask import Flask, request
 import nominatim
 import valhalla
@@ -5,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/optimise', methods=["POST"])
 def optimise_addreses():
+    """Process the the requests addresses and return JSON of the addresses in optimised order"""
     request_data = request.get_json()
     addresses = request_data['addresses']
 
