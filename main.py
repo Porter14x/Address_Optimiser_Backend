970e1ae47495c48ab05d121a2ea680e9ae6d2127
Porter14x <raytporter@gmail.com>
2025-08-16 17:18:53 +0100

    linting main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-"""Main.py is responsible for handling requests via Flask and calling functions from other modules to satisfy the requests"""
+"""Main.py is responsible for handling requests via Flask 
+and calling functions from other modules to satisfy the requests"""
 
 from flask import Flask, request
 import nominatim
@@ -10,10 +11,8 @@ def optimise_addreses():
     """Process the the requests addresses and return JSON of the addresses in optimised order"""
     request_data = request.get_json()
     addresses = request_data['addresses']
-
     geos = nominatim.geocode_adds(addresses)
     opt_adds = valhalla.optimise_adds(geos)
-
     return opt_adds
 
 if __name__=='__main__': 
