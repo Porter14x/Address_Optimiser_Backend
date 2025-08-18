d54a569b2ee4c8da8e8adb7ec16e90417345e52f
Porter14x <raytporter@gmail.com>
2025-08-18 20:24:34 +0100

    Issue #4 - Added value insertion and tests for db, cursors now open and close for each function they are used

--- database.py
+++ database.py
@@ -2,13 +2,19 @@
 
 import sqlite3
 import re
+import atexit
 
 DB_PATH = "rounds.db"
+FORBIDDEN_CHAR = [";", '"',] # list of chars that should never appear in inputs
 
 con = sqlite3.connect(DB_PATH)
-cur = con.cursor()
 
-def create_table(table):
+def close_con():
+    con.close()
+
+atexit.register(close_con)
+
+def create_table(table, cur):
     """Create a table with the passed name if it doesn't exist"""
 
     #ensure only letters, nums & underscores are in table name eg no space for 'DROP TABLE'
@@ -23,4 +29,29 @@ def create_table(table):
 
     creation = f"CREATE TABLE {table}(street VARCHAR(255), postcode VARCHAR(10))"
     cur.execute(creation)
+    con.commit()
     return f"Table {table} created"
+
+def insert_table(table, street, postcode, cur):
+    """Insert street and postcode values into desired table"""
+
+    all_table = [t[0] for t in cur.execute("SELECT name FROM sqlite_master").fetchall()]
+
+    for char in FORBIDDEN_CHAR:
+        if char in street or char in postcode:
+            return f"Forbidden character {char} in input"
+
+    rb = f"{table}_rb"
+    sql_rb = f"CREATE TABLE {rb} AS SELECT * FROM {table}"
+
+    if rb in all_table:
+        sql_drop = f"DROP TABLE {rb}"
+        cur.execute(sql_drop)
+        cur.execute(sql_rb)
+    else:
+        cur.execute(sql_rb)
+    
+    sql_in = f"INSERT INTO {table} VALUES (?, ?)"
+    cur.execute(sql_in, (street, postcode))
+    con.commit()
+    return f"Inserted values ({street}, {postcode}) into {table}"
--- test.py
+++ test.py
@@ -8,32 +8,90 @@ class TestDatabaseMethods(unittest.TestCase):
     """Class for testing the functions of database.py"""
 
     con = sqlite3.connect("test.db")
-    cur = con.cursor()
 
     def setUp(self):
         """ensure dummy table is wiped"""
-        self.cur.execute("DROP TABLE IF EXISTS dummy;")
+        cur = self.con.cursor()
+        cur.execute("DROP TABLE IF EXISTS dummy;")
+        cur.close()
         return super().setUp()
 
     def test_create_table(self):
+        cur = self.con.cursor()
         """test create_table table creation and sanitation"""
         regex_fail_list = ["!", '"', "£", "$", "%", "^", "&", "*", "(", ")", "-", "+","=",
                            "{", "}", "[", "]", "~", "#", ":", ";", "@", "'", "<", ",",
                            ">", ".", "?", "/", "|", "¬", "`",]
 
         rb_fail = "table_rb"
-        self.assertEqual(d.create_table(rb_fail), "Cannot have _rb in table name")
+        self.assertEqual(d.create_table(rb_fail, cur), "Cannot have _rb in table name")
 
         for char in regex_fail_list:
-            self.assertEqual(d.create_table("table"+char),
+            self.assertEqual(d.create_table("table"+char, cur),
             "Invalid table name. Please ensure only letters, numbers and underscores are used")
 
-        d.create_table("dummy")
-        self.assertEqual(d.create_table("dummy"), "Table dummy already exists")
+        d.create_table("dummy", cur)
+        self.assertEqual(d.create_table("dummy", cur), "Table dummy already exists")
+
+        cur.close()
+
+    def test_insert_table(self):
+        """test insert_table value insertion and sanitation"""
+        cur = self.con.cursor()
+        cur.execute("DROP TABLE IF EXISTS dummy;")
+
+        d.create_table("dummy", cur)
+
+        FORBIDDEN_CHAR = [";", '"',]
+        for char in FORBIDDEN_CHAR:
+            self.assertEqual(d.insert_table("dummy", "street", f"{char}postcode", cur),
+                             f"Forbidden character {char} in input")
+            self.assertEqual(d.insert_table("dummy", f"{char}street", "postcode", cur),
+                             f"Forbidden character {char} in input")
+            self.assertEqual(d.insert_table("dummy", f"{char}street", f"{char}postcode", cur),
+                             f"Forbidden character {char} in input")
+        
+        output = d.insert_table("dummy", "1 House St", "A01", cur)
+        self.assertEqual(output, "Inserted values (1 House St, A01) into dummy")
+        result_dummy = cur.execute("SELECT * FROM dummy").fetchall()
+        result_dummy_rb = cur.execute("SELECT * FROM dummy_rb").fetchall()
+        all_tables = [r[0] for r in
+                      cur.execute("SELECT name FROM sqlite_master").fetchall()]
+        
+        self.assertListEqual(result_dummy, [("1 House St", "A01")])
+        self.assertListEqual(result_dummy_rb, [])
+        self.assertListEqual(all_tables, ["dummy", "dummy_rb"])
+
+        output = d.insert_table("dummy", "2 House St", "A01", cur)
+        self.assertEqual(output, "Inserted values (2 House St, A01) into dummy")
+        result_dummy = cur.execute("SELECT * FROM dummy").fetchall()
+        result_dummy_rb = cur.execute("SELECT * FROM dummy_rb").fetchall()
+        all_tables = [r[0] for r in
+                      cur.execute("SELECT name FROM sqlite_master").fetchall()]
+
+        self.assertListEqual(result_dummy, [("1 House St", "A01"), ("2 House St", "A01")])
+        self.assertListEqual(result_dummy_rb, [("1 House St", "A01")])
+        self.assertListEqual(all_tables, ["dummy", "dummy_rb"])
+
+        output = d.insert_table("dummy", "3 House St", "A01", cur)
+        self.assertEqual(output, "Inserted values (3 House St, A01) into dummy")
+        result_dummy = cur.execute("SELECT * FROM dummy").fetchall()
+        result_dummy_rb = cur.execute("SELECT * FROM dummy_rb").fetchall()
+        all_tables = [r[0] for r in
+                      cur.execute("SELECT name FROM sqlite_master").fetchall()]
+
+        self.assertListEqual(result_dummy, [("1 House St", "A01"), ("2 House St", "A01"),
+                                            ("3 House St", "A01")])
+        self.assertListEqual(result_dummy_rb, [("1 House St", "A01"), ("2 House St", "A01")])
+        self.assertListEqual(all_tables, ["dummy", "dummy_rb"])
+        
+        cur.close()
 
     def tearDown(self):
-        """close connection"""
-        self.cur.close()
+        """double check dummy table wiped"""
+        cur = self.con.cursor()
+        cur.execute("DROP TABLE IF EXISTS dummy;")
+        cur.close()
         return super().tearDown()
 
 if __name__ == '__main__':
